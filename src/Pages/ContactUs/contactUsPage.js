92e839e9b90008e5e665b3dd48191b8e2e1c5161
Saydullo <https://github.com/saydullo0502>
2023-05-10 16:30:38 +0500

    versal

--- src/Pages/ContactUs/contactUsPage.js
+++ src/Pages/ContactUs/contactUsPage.js
@@ -27,7 +27,7 @@ function ContactUsPage() {
       `@online_react_shop_bot 🎯%0A`,
       `Name: ${data.first_name}%0A`,
       `Email: ${data.email}%0A`,
-      `Message: ${data.message}%0A`,
+      `Telfon: ${data.telfon}%0A`,
     ];
     const botMessege = w + f + e + m;
     // setLoader(true);
@@ -35,12 +35,12 @@ function ContactUsPage() {
       `${TELEGRAM_API}/${BOT_TOKEN}/sendMessage?${CHAT_ID}&text=${botMessege}&${BOT_CHAT_TYPE}`
     ).then(({ data }) => {
       if (data?.ok) {
-        toast.success("Omad");
+        toast.success("Muvaffaqiyatli");
         // setLoader(false);
         reset();
       }
       if (!data?.ok) {
-        toast.error("Odda boldi");
+        toast.error("Muvaffaqiyatsiz");
         // setLoader(false);
       }
     });
@@ -89,14 +89,23 @@ function ContactUsPage() {
               />
             </div>
 
-            <div className="person_contact">
-              <p>Xabar</p>
+        
+              {/* <p>Xabar</p> */}
               {/* <input type="email" placeholder="Xabaringiz" /> */}
-              <textarea
+              {/* <textarea
                 {...register("message", { required: true })}
                 placeholder="Xabaringiz"
-              ></textarea>
-            </div>
+              ></textarea> */}
+              <div className="person_contact">
+                <p>Telefon raqami</p>
+                <input
+                  type="tel"
+                  placeholder="Sizning raqamingiz"
+                  required
+                  {...register("telfon", { required: true })}
+                />
+              </div>
+           
 
             <div className="person_contact_button">
               <button>So'rov yuboring</button>
